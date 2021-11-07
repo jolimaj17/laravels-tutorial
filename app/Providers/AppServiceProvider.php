69f405c631babbf715ac7f2baa55d9ef32bd5f32
jolimaj <jolima.javin@bitnacs.com>
2021-11-07 20:15:22 +0800

    sd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -25,11 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      if (!App::environment([
-        'local',
-        'testing',
-    ])) {
-        URL::forceScheme('https');
-    }
+      URL::forceScheme('https');
     }
 }
