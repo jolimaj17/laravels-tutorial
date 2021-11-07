1e9d863466049b7c1cee1babb496eee20a5c4a3f
jolimaj <jolima.javin@bitnacs.com>
2021-11-07 19:15:10 +0800

    fix

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+if (App::environment('production')) {
+  URL::forceScheme('https');
+}
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
